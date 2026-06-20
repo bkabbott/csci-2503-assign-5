206ad4f9069a52581c7e88bb779e5e5a9ef4eb46
Brian Abbott <hi@brianabbott.com>
2026-06-20 12:20:48 -0400

    added comments, added toString() method, added accessors and mutators with nodiscard set for getters, per intellij, added main method to function equivalent to the Java class

--- Fan.cpp
+++ Fan.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <string>
+#include <format>
+#include <iostream>
 
 struct Fan
 {
@@ -10,11 +12,39 @@ struct Fan
     double radius { 1.53 };
     std::string color { "green" };
 
+    // sets members to default values above
     Fan() = default;
 
+    // convenience constructor
     explicit Fan(double newRadius) : radius{ newRadius } {}
+
+    std::string toString() const
+    {
+        return std::format("A {:.2f} inch {} fan at a speed of {}",
+                           radius, color, speed);
+    }
+
+    // accessors and mutators
+    [[nodiscard]] int         getSpeed()  const            { return speed;  }
+    void        setSpeed(int s)              { speed  = s;    }
+    [[nodiscard]] double      getRadius() const            { return radius; }
+    void        setRadius(double r)          { radius = r;    }
+    [[nodiscard]] std::string getColor()  const            { return color;  }
+    void        setColor(const std::string& c) { color = c;   }
 };
 
+
+
 int main(void) {
-    Fan fan;
+    Fan defaultFan;
+    Fan customFan{ 8.0 };
+
+    // Mutators as with Java
+    customFan.setSpeed(3);
+    customFan.setColor("blue");
+
+    std::cout << defaultFan.toString() << '\n';
+    std::cout << customFan.toString()  << '\n';
+
+    return 0;
 }
